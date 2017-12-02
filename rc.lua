397ce207b020f249134110901a400c528bbe44aa
Disrado <n.maltsev.dis@gmail.com>
2017-12-02 04:53:24 +0200

    2.12.2017 version

--- rc.lua
+++ rc.lua
@@ -38,7 +38,7 @@ end
 
 -- {{{ Variable definitions
 -- Themes define colours, icons, font and wallpapers.
-beautiful.init(awful.util.get_themes_dir() .. "/zenburn/theme.lua")
+beautiful.init("~/.config/awesome/themes/zenburn/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
 terminal = "terminator"
@@ -55,18 +55,18 @@ modkey = "Mod4"
 -- Table of layouts to cover with awful.layout.inc, order matters.
 awful.layout.layouts = {
 --    awful.layout.suit.floating,
-    awful.layout.suit.tile,
+   awful.layout.suit.tile,
 --    awful.layout.suit.tile.left,
---    awful.layout.suit.tile.bottom,
+   awful.layout.suit.tile.bottom,
 --    awful.layout.suit.tile.top,
 --    awful.layout.suit.fair,
---    awful.layout.suit.fair.horizontal,
+   awful.layout.suit.fair.horizontal,
 --    awful.layout.suit.spiral,
 --    awful.layout.suit.spiral.dwindle,
 --    awful.layout.suit.max,
-    awful.layout.suit.max.fullscreen,
+   awful.layout.suit.max.fullscreen,
 --    awful.layout.suit.magnifier,
- --   awful.layout.suit.corner.nw,
+   awful.layout.suit.corner.nw,
     -- awful.layout.suit.corner.ne,
     -- awful.layout.suit.corner.sw,
     -- awful.layout.suit.corner.se,
@@ -476,7 +476,7 @@ awful.rules.rules = {
     { rule = { class = "Emacs" },
       properties = { screen = 1, tag = " 2" } },
     
-    { rule = { class = "Firefox" },
+    { rule = { class = "Chromium" },
       properties = { screen = 1, tag = " 4" } },
 }
 -- }}}
